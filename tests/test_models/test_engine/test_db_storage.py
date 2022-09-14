de3b9b274f30445cddd725f5b17c0727c91489dd
gbrownee <99354783+gbrownee@users.noreply.github.com>
2022-09-14 13:19:28 +0100

    Create test_db_storage.py

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Unittests for db_storage
+"""
+import unittest
+
+
+class TestDBStorage(unittest.TestCase):
+    """
+    Tests for SQLAlchemy storage method.
+    """
+
+    def test_all(self):
+        """
+        tests if all works in File Storage
+        """
+        pass
