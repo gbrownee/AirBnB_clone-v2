30eaffd47f2e9e0b7e4b1559e82a47a4cf89521b
gbrownee <99354783+gbrownee@users.noreply.github.com>
2022-09-14 13:11:18 +0100

    update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+State Module for HBNB project
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+# from models import storage
+# from models import City
+import os
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    This is the class for State
+    """
+
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='state', cascade='delete')
+
+    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+        @property
+        def all_cities(self):
+            """
+            getter for cities
+            """
+            cities = []
+            insta = storage.all(City)
+            for value in insta.values():
+                if value.state_id == self.id:
+                    cities.append(value)
+            return cities
